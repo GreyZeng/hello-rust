e3ad5a0503fb7d3808a0eb2a387a0a7c782ccfcc
GreyZeng <410486047@qq.com>
2020-12-18 13:51:20 +0800

    add note

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::thread;
+use std::time::Duration;
+use std::rc::Rc;
+
 struct Point {
     x: i32,
     y: i32,
@@ -14,6 +18,28 @@ fn f(p: &mut Point) {
     p.x = 3;
 }
 
+fn new_thread() {
+    let p1 = Point { x: 245, y: 23 };
+    let h = thread::spawn(move || {
+        for i in 0..10 {
+            // thread::sleep(Duration::from_millis(100));
+            println!("{} {}", p1.x, p1.y);
+        }
+    });
+
+    h.join();
+}
+
 fn main() {
-    simple_borrow();
+    // Box保证分配到堆,只能有一个所有权人
+    let p = Box::new(Point { x: 23, y: 32 });
+    println!("{}", (*p).x);
+    println!("{}", p.x);
+    // new_thread();
+
+    let p2 = Rc::new(Point { x: 3, y: 34 });
+    let p3 = Rc::clone(&p2);
+    let p4 = Rc::clone(&p2);
+    println!("{} {} {}", p2.x, p3.x, p4.x);
+    // simple_borrow();
 }
