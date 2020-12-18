0d691aeec57b5eff5ffeb311fa9de6588b76788e
GreyZeng <410486047@qq.com>
2020-12-18 13:31:50 +0800

    rust note

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+fn simple_borrow() {
+    let mut p1 = Point { x: 25, y: 24 };
+    f(&mut p1); // p1的所有权转移到p，引出借用的概念
+    println!("{} {}", p1.x, p1.y);
+}
+
+// 只读借用,如果需要修改，一定要加mut
+fn f(p: &mut Point) {
+    p.x = 3;
+}
 
 fn main() {
-    let x = 5;
-    let x = x + 1;
-    let x = x * 2;
-    println!("The value of x is: {}", x);
+    simple_borrow();
 }
