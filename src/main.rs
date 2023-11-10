b8b4b384096456f914ec00cd877382b8be3d4454
GreyZeng <410486047@qq.com>
2023-11-10 15:54:08 +0800

    add unit test

--- src/main.rs
+++ src/main.rs
@@ -17,3 +17,10 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
     // Rust 中有 return 语句，但是这里的gcd函数不需要，如果一个函数以没有尾随着分号的表达式结尾，那么这个表达式就是函数的返回值
     n
 }
+
+// Rust 内置单元测试
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11)
+}
